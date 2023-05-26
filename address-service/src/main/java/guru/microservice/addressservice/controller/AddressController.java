bd154d994870d1de0e4cc272ae50a6edddf15475
meher12 <meher.khemiri@gmail.com>
2023-05-26 10:24:02 +0100

    replace feignClient instead of RestTemplate

--- address-service/src/main/java/guru/microservice/addressservice/controller/AddressController.java
+++ address-service/src/main/java/guru/microservice/addressservice/controller/AddressController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "api/v1/address")
+@RequestMapping(value = "/api/v1/address")
 @RefreshScope
 public class AddressController {
 
