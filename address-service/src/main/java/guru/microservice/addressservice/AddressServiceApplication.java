dfa5595ee561dc5adf3e6f1b55970eed11bc4772
meher12 <meher.khemiri@gmail.com>
2023-05-30 19:24:29 +0100

    update project

--- address-service/src/main/java/guru/microservice/addressservice/AddressServiceApplication.java
+++ address-service/src/main/java/guru/microservice/addressservice/AddressServiceApplication.java
@@ -2,8 +2,10 @@ package guru.microservice.addressservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class AddressServiceApplication {
 
 	public static void main(String[] args) {
